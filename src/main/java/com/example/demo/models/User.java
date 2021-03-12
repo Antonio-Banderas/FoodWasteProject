c009e313b6dd42f7861a0962b636485099c6a46e
JuliusPanduro <j.panduro.r@hotmail.com>
2021-03-12 10:44:39 +0100

    First commit User

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,9 +1,20 @@
 package com.example.demo.models;
 
 /**
- * @author Carsten
+ * @author Julius Panduro
  * Time: 09.52
  * Date: 12/03/2021
  */
 public class User {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int zipCode;
+
+    public User(String firstName, String lastName, String address, int zipCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.zipCode = zipCode;
+    }
 }
